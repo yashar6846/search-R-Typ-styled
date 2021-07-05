7f1740322bfed029ca3c61e3f72e7830735bcb45
yashar6846 <yasharnajafi6846@gmail.com>
2021-07-05 11:06:18 +0300

    commit joke

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
-import { Wrapper, Row, Header, Image } from "./components/styled/index"
+import React,  { useState } from 'react'
+import axios from "axios";
+import JokeItem from "./components/styled/JokeItem";
+import {
+   Wrapper,
+   Row,
+    Header, 
+    Image,
+     Search, 
+     Form,
+    Button,
+   }from "./components/styled/index";
+   import { Joke } from "./common/types";
+
 import owl from './images/owl.svg'
+
+const BASE_URL = "https://v2.jokeapi.dev/joke/Any";
+
 const App = () => {
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+  const [jokes, setJokes] = useState<Joke[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+       setSearch(e.target.value);
+  };
+  const getJokes = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const ENDPOINT = `${BASE_URL}?contains=${search}&amount=10`;
+
+    const { data } = await axios.get(ENDPOINT);
+    if (data.error) {
+      setError(true);
+      setJokes([]);
+    } else {
+      setError(false);
+      setJokes(data.jokes);
+    }
+
+    setSearch("");
+  };
+  
+
     return(
      <div>
        <Wrapper>
@@ -9,7 +49,20 @@ const App = () => {
            <Header>Joker</Header>
            <Image src={owl} alt="Baykus" />
          </Row>
-        <h1>baykos</h1>
+        <Form onSubmit={getJokes}>
+          <Search type="text"
+          placeholder="Search.."
+          value={search}
+          onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </Form>
+          {/* Jokes */}
+          <div>
+          {error && <p>Sorry, no jokes found.</p>}
+          {jokes.length > 0 &&
+            jokes.map((joke) => <JokeItem key={joke.id} joke={joke} />)}
+        </div>
         </Wrapper>
      </div>
     )
--- src/components/styled/index.ts
+++ src/components/styled/index.ts
@@ -2,5 +2,12 @@ import Wrapper from './Wrapper';
 import Row from './Row';
 import Header from './Header'
 import Image from './Image'
+import Search from './Search'
+import Form from './Form'
+import Button from './Button'
+import {CardWrapper, CardTop, CardBottom, Setup, Delivery} from './Card'
 
-export { Wrapper, Row, Header, Image };
+export { Wrapper, Row, Header,
+     Image, Form, Search, 
+    Button,CardWrapper, CardTop,
+     CardBottom, Setup, Delivery };
